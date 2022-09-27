3d85bf84432d4cafb7641fc15c0f6324c9a10eea
Venus <bisnesbrend@gmail.com>
2022-09-27 22:45:50 +0300

    Добавьте файлы проекта.

---
+++ Admin/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Admin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey current = Registry.CurrentUser;
+            RegistryKey create = current.CreateSubKey("Porno", true);
+
+            Console.Write("Введите цвет задника\n> ");
+            create.SetValue("window", Console.ReadLine());
+
+            Console.Write("Введите цвет текста\n> ");
+            create.SetValue("button", Console.ReadLine());
+
+            create.Close();
+        }
+    }
+}
---
+++ PornoHub/Form1.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PornoHub
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegistryKey current = Registry.CurrentUser;
+            RegistryKey open = current.OpenSubKey("Porno");
+
+
+            try
+            {
+                Color window = Color.FromName(open.GetValue("window").ToString());
+                Color buttonAndText = Color.FromName(open.GetValue("button").ToString());
+
+                tableLayoutPanel1.BackColor = window;
+
+                button1.ForeColor = buttonAndText;
+                label1.ForeColor = buttonAndText;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Тест на возраст не пройден");
+            }
+        }
+    }
+}
